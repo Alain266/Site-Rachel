66a0965ef5302dec0f119b5be6698cba211a14c4
Alain266 <a.okala26@gmail.com>
2025-01-29 22:36:12 +0100

    path debug 2.0

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ document.addEventListener("DOMContentLoaded", function () {
     const gallery = document.querySelector(".grid")
 
     const images = [
-        "/assets/images/image1.jpg",
-        "/assets/images/image2.jpg",
-        "/assets/images/image3.jpg",
-        "/assets/images/image4.jpg",
-        "/assets/images/image5.jpg",
-        "/assets/images/image6.jpg"
+        "assets/images/image1.jpg",
+        "assets/images/image2.jpg",
+        "assets/images/image3.jpg",
+        "assets/images/image4.jpg",
+        "assets/images/image5.jpg",
+        "assets/images/image6.jpg"
     ]
 
     images.forEach((src) => {
